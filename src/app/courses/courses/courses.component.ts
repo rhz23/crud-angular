6fdb1c7965ccdd58a1294afd729ab79ef806d0b8
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2024-01-01 19:23:24 -0400

    Table and Course List

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit{
+
+  courses:Course[] = [
+    { _id: '1', name: 'Angular', category: 'front-end'}
+  ];
+  displayedColumns = ['name', 'category'];
+
+  Constructor() {
+    // this.courses = [];
+
+  }
+
+  ngOnInit(): void {
+
+
+  }
+
+
+}
